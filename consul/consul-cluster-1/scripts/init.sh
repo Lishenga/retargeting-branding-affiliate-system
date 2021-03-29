4db43835ba85130347f16d96d57642a740e5417c
Lishenga <lishengaleon@gmail.com>
2021-03-29 16:55:17 +0300

    consul-agent-one

---
+++ consul/consul-cluster-1/scripts/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Set variables
+# OUR_SERVER_ADDRESS=$(ip addr show eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+
+# # Collect sample json file reboot
+# # retry users
+# SAMPLE_FILE=$(cat ./opt/consul/configs/consul.sample.json)
+
+# # Read in template one line at the time, and replace variables (more natural (and efficient) way, thanks to Jonathan Leffler).
+# JSON_STRING=$(jq -n --arg snadress "$OUR_SERVER_ADDRESS" "$SAMPLE_FILE")
+
+# echo $JSON_STRING >> /etc/consul.d/consul.json
+
+# Initialize the consul agent
+# envoy -c /etc/envoy/envoy.yaml
+consul agent -retry-join "provider=aws tag_key=Name tag_value=ECS Instance - EC2ContainerService-consul-server" -config-file=/etc/consul.d/consul.json -retry-interval=5s
