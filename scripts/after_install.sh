40c368184f43e150b793c0e9e8bd337216fd3bb1
Lishenga <lishengaleon@gmail.com>
2021-04-30 21:27:37 +0300

    working

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 
-docker-compose up -d --build
+cd /home/ubuntu/retargeting-branding-affiliate-system
+export DOCKER_CLIENT_TIMEOUT=300
+export COMPOSE_HTTP_TIMEOUT=300    
+sudo docker-compose -f docker-compose.yml up -d --build
